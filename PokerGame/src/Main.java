daaa23d5ae1640745c290662fbe3c09c1297a3b8
lucablack2007 <68418123+LucaBlack2007@users.noreply.github.com>
2024-10-18 11:35:11 -0700

    Add files via upload

--- PokerGame/src/Main.java
+++ PokerGame/src/Main.java
@@ -6,14 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         List<Hand> hands = new ArrayList<>();
+
         Map<Hand, Integer> handOccurance = new HashMap<>();
         Map<String, Integer> handWinAmount = new HashMap<>();
         Map<String, Integer> handPulledAmount = new HashMap<>();
+        Map<String, Map<Hand, Integer>> winningHandMap = new HashMap<>();
+
         for (int i = 0; i < 1000000; i++) {
             List<Player> players = new ArrayList<>();
             initializeDeck();
             communityCards = new ArrayList<>();
-            for (int p = 0; p < 8; p++) {
+            for (int p = 0; p < 14; p++) {
                 Player ply = new Player();
                 ply.createHand();
                 players.add(ply);
@@ -25,7 +28,6 @@ public class Main {
 
             Map<Player, Hand> bestHands = new HashMap<>();
             for (Player player : players) {
-              //  System.out.println(player.printHand());
 
                 List<Card> hand = player.getHand();
                 boolean isSuited = hand.get(0).suit() == hand.get(1).suit();
@@ -57,12 +59,24 @@ public class Main {
                         + (hand.get(0).rankValue() > hand.get(1).rankValue() ? hand.get(1).rank() : hand.get(0).rank())
                         + (isSuited ? "s" : "");
 
-
+//Map<String, Map<Hand, Integer>> winningHandMap = new HashMap<>();
                // System.out.println(handString);
                 if (handWinAmount.containsKey(handString)) {
                     handWinAmount.put(handString, handWinAmount.get(handString) + 1);
                 } else {
                     handWinAmount.put(handString, 1);
+                }
+
+                if (winningHandMap.containsKey(handString)) {
+                    if (winningHandMap.get(handString).containsKey(winnerHand)) {
+                        winningHandMap.get(handString).put(winnerHand, winningHandMap.get(handString).get(winnerHand) + 1);
+                    } else {
+                        winningHandMap.get(handString).put(winnerHand, 1);
+                    }
+                } else {
+                    Map<Hand, Integer> tempMap = new HashMap<>();
+                    tempMap.put(winnerHand, 1);
+                    winningHandMap.put(handString, tempMap);
                 }
 
                 System.out.print("\rTested " + i + " hands, winner: " + handString + " (" + Math.round((double)handWinAmount.get(handString) / (double)handPulledAmount.get(handString) * 100) + "% win), hand: " + winnerHand);
@@ -82,12 +96,23 @@ public class Main {
                 handWinAmount.put(handString, 0);
             }
         }
-
+        System.out.println("Hand|Times Had|Times Won|Win %|High Card|Pair|Two Pair|Trips|Straight|Flush|Full House|Quad|Straight Flush|Royal Flush");
         for (String handString : handPulledAmount.keySet()) {
+            //winningHandMap.get(handString).put(winnerHand, winningHandMap.get(handString).get(winnerHand) + 1);
             System.out.println(handString + "|"
                     + handPulledAmount.get(handString) + "|"
                     + handWinAmount.get(handString) + "|"
-                    + (handWinAmount.get(handString) / handPulledAmount.get(handString) * 100) + "%"
+                    + ((double)handWinAmount.get(handString) / (double)handPulledAmount.get(handString) * 100) + "%|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.HIGH_CARD, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.PAIR, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.TWO_PAIR, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.SET, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.STRAIGHT, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.FLUSH, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.FULL_HOUSE, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.QUAD, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.STRAIGHT_FLUSH, 0) + "|"
+                    + winningHandMap.get(handString).getOrDefault(Hand.ROYAL_FLUSH, 0)
             );
         }
 
@@ -138,6 +163,49 @@ public class Main {
             System.out.print(card.print() + " ");
         }
         System.out.println();
+    }
+
+
+    private static Card card(Suit suit, int rank) { return new Card(rank, suit); }
+    public static void testMain(String[] args) {
+
+
+        communityCards = Arrays.asList(
+                card(Suit.CLUB, 10),
+                card(Suit.CLUB, 9),
+                card(Suit.CLUB, 8),
+                card(Suit.CLUB, 6),
+                card(Suit.SPADE, 10)
+        );
+
+        Player player1 = new Player(Arrays.asList(card(Suit.HEART, 10), card(Suit.DIAMOND, 10))); // quad
+        Player player2 = new Player(Arrays.asList(card(Suit.CLUB, 5), card(Suit.CLUB, 7))); // straight flush
+        Player player3 = new Player(Arrays.asList(card(Suit.HEART, 9), card(Suit.DIAMOND, 9))); // full house
+        Player player4 = new Player(Arrays.asList(card(Suit.HEART, 6), card(Suit.DIAMOND, 6))); // full house
+        Player player5 = new Player(Arrays.asList(card(Suit.HEART, 2), card(Suit.DIAMOND, 14))); // pair (on board)
+        Player player6 = new Player(Arrays.asList(card(Suit.HEART, 8), card(Suit.SPADE, 13))); // two pair (one on baord)
+        Player player7 = new Player(Arrays.asList(card(Suit.HEART, 11), card(Suit.CLUB, 14))); // flush
+        Player player8 = new Player(Arrays.asList(card(Suit.DIAMOND, 5), card(Suit.DIAMOND, 7))); // straight
+
+
+        Hand hand1 = HandEvaluator.evaluateHand(player1.getHand(), communityCards);
+        Hand hand2 = HandEvaluator.evaluateHand(player2.getHand(), communityCards);
+        Hand hand3 = HandEvaluator.evaluateHand(player3.getHand(), communityCards);
+        Hand hand4 = HandEvaluator.evaluateHand(player4.getHand(), communityCards);
+        Hand hand5 = HandEvaluator.evaluateHand(player5.getHand(), communityCards);
+        Hand hand6 = HandEvaluator.evaluateHand(player6.getHand(), communityCards);
+        Hand hand7 = HandEvaluator.evaluateHand(player7.getHand(), communityCards);
+        Hand hand8 = HandEvaluator.evaluateHand(player8.getHand(), communityCards);
+
+        System.out.println(hand1);
+        System.out.println(hand2);
+        System.out.println(hand3);
+        System.out.println(hand4);
+        System.out.println(hand5);
+        System.out.println(hand6);
+        System.out.println(hand7);
+        System.out.println(hand8);
+
     }
 }
 
